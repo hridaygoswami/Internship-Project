01bec349491f6d0b6e5d38d295e90f6bf4a11356
hridaygoswami <epikhriday@gmail.com>
2023-08-19 10:42:49 +0530

    added requirements

--- websiteUI/views.py
+++ websiteUI/views.py
@@ -11,13 +11,13 @@ def result(request):
     inp1 = request.POST['float-input1']
     inp2 = request.POST['float-input2']
     inp3 = request.POST['float-input3']
-    url = requests.get(f"http://127.0.0.1:8080/prediction_api?LSTAT={inp1}&RM={inp2}&PTRATIO={inp3}")
-    print(url.json()*1000)
+    # url = requests.get(f"http://127.0.0.1:8080/prediction_api?LSTAT={inp1}&RM={inp2}&PTRATIO={inp3}")
+    # print(url.json()*1000)
     print(inp1, inp2, inp3)
     arr.append(inp1)
     arr.append(inp2)
     arr.append(inp3)
     return render(request, 'result.html', {
-        "val": url.json()*1000
-        # "val":inp1
+        # "val": url.json()*1000
+        "val":inp1
     })
